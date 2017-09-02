8ac4784b8dc6f8369503a6a0a4cafa5fe6d0e5b0
Mike Amundsen <mamund@yahoo.com>
2017-09-02 18:28:02 -0400

    direct

---
+++ direct/server.js
@@ -0,0 +1,21 @@
+/* graphql */
+/* http://graphql.org/graphql-js/ */
+
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello:String
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+graphql(schema, '{hello}', root).then((response) => {
+  console.log(response);
+});
+
