2979d9025bc351fd4a166a36c797ee7ae6226f90
Mike Amundsen <mamund@yahoo.com>
2017-09-03 18:54:23 -0400

    object-types

---
+++ object-types/server.js
@@ -0,0 +1,56 @@
+/* object types */
+/* http://graphql.org/graphql-js/object-types/ */
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// graphql schema
+var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`);
+
+// implement RandomDie graphql type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// root resolver
+var root = {
+  getDie: function({numSides}) {
+    return new RandomDie(numSides || 6);
+  }
+}
+
+// express app
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
+
